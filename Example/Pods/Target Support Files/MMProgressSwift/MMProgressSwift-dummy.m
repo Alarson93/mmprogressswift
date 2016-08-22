971e7037beb4b1f8be0003053e24f817c1a9d7ca
Alex Larson <alarson@myriadmobile.com>
2016-08-22 14:23:09 -0500

    Updated pods

--- Example/Pods/Target Support Files/MMProgressSwift/MMProgressSwift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MMProgressSwift : NSObject
-@end
-@implementation PodsDummy_MMProgressSwift
-@end
--- Example/Pods/Target Support Files/MMProgressSwift/MMProgressSwift-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double MMProgressSwiftVersionNumber;
-FOUNDATION_EXPORT const unsigned char MMProgressSwiftVersionString[];
-
